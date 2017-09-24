69e56414f9d77f65b2f6029136e7afe06c3b4126
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2017-09-24 19:53:06 +0900

    commit_final

---
+++ project1/synch.h
@@ -0,0 +1,53 @@
+#ifndef THREADS_SYNCH_H
+#define THREADS_SYNCH_H
+
+#include <list.h>
+#include <stdbool.h>
+
+/* A counting semaphore. */
+struct semaphore 
+  {
+    unsigned value;             /* Current value. */
+    struct list waiters;        /* List of waiting threads. */
+  };
+
+void sema_init (struct semaphore *, unsigned value);
+void sema_down (struct semaphore *);
+bool sema_try_down (struct semaphore *);
+void sema_up (struct semaphore *);
+void sema_self_test (void);
+
+/* Lock. */
+struct lock 
+  {
+    struct thread *holder;      /* Thread holding lock (for debugging). */
+    struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem; // edit_ps3 end
+  };
+
+void lock_init (struct lock *);
+void lock_acquire (struct lock *);
+void lock_priority_donation (struct lock *lock); //edit_ps2 end
+bool lock_try_acquire (struct lock *);
+void lock_release (struct lock *);
+void lock_priority_revert (struct lock *lock); //edit_ps2 end
+bool lock_held_by_current_thread (const struct lock *);
+/* Condition variable. */
+struct condition 
+  {
+    struct list waiters;        /* List of waiting threads. */
+  };
+
+void cond_init (struct condition *);
+void cond_wait (struct condition *, struct lock *);
+void cond_signal (struct condition *, struct lock *);
+void cond_broadcast (struct condition *, struct lock *);
+
+/* Optimization barrier.
+
+   The compiler will not reorder operations across an
+   optimization barrier.  See "Optimization Barriers" in the
+   reference guide for more information.*/
+#define barrier() asm volatile ("" : : : "memory")
+
+#endif /* threads/synch.h */
